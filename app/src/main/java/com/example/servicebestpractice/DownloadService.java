d4c7ed5dc67a639d565d5d7f20214e90b4ad6553
15291783517 <848671907@qq.com>
2017-08-31 18:54:53 +0800

    first commit

---
+++ app/src/main/java/com/example/servicebestpractice/DownloadService.java
@@ -0,0 +1,127 @@
+package com.example.servicebestpractice;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.webkit.DownloadListener;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class DownloadService extends Service {
+
+    private DownloadTask downloadTask;
+
+    private String downloadUrl;
+
+    private DownloadListener listener = new DownloadListener() {
+        @Override
+        public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+
+        }
+
+        @Override
+        public void onProgress(int progress) {
+            getNotificationManager().notify(1, getNotification("Downloading...", progress));
+        }
+
+        @Override
+        public void onSuccess() {
+            downloadTask = null;
+            //下载成功时将前台服务关闭，并创建一个下载成功的通知
+            stopForeground(true);
+            getNotificationManager().notify(1, getNotification("Download Success", -1));
+            Toast.makeText(DownloadService.this, "Download Success", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onFailed() {
+            downloadTask = null;
+            //下载成功时将前台服务关闭，并创建一个下载成功的通知
+            stopForeground(true);
+            getNotificationManager().notify(1, getNotification("Download Failed", -1));
+            Toast.makeText(DownloadService.this, "Download Failed", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPause() {
+            downloadTask = null;
+            Toast.makeText(DownloadService.this, "Pause", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    class DownloadBinder extends Binder {
+
+        public void startDownload(String url) {
+            if (downloadTask == null) {
+                downloadUrl = url;
+                downloadTask = new DownloadTask(listener);
+                downloadTask.execute(downloadUrl);
+                startForeground(1, getNotification("Download...", 0));
+                Toast.makeText(DownloadService.this, "Downloading...", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        public void pauseDownload() {
+            if (downloadTask != null) {
+                downloadTask.pauseDownload();
+            }
+        }
+
+        public void cancelDownload() {
+            if (downloadTask != null) {
+                downloadTask.cancelDownload();
+            } else {
+                if (downloadUrl != null) {
+                    //取消下载时需将文件删除，并将通知关闭
+                    String fileName = downloadUrl.substring(downloadUrl.lastIndexOf("/"));
+                    String directory = Environment.getExternalStorageDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+                    File file = new File(directory + fileName);
+                    if (file.exists()) {
+                        file.delete();
+                    }
+                    getNotificationManager().cancel(1);
+                    stopForeground(true);
+                    Toast.makeText(DownloadService.this, "Canceled", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private NotificationManager getNotificationManager() {
+        return (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    private Notification getNotification(String title, int progress) {
+
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivities(this,0, new Intent[]{intent},0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+        builder.setContentIntent(pi);
+        builder.setContentTitle(title);
+        if (progress >= 0){
+            //当progress大于或等于0时才需显示下载进度
+            builder.setContentText(progress + "%");
+            builder.setProgress(100,progress,false);
+        }
+        return builder.build();
+    }
+}
+
